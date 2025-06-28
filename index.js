8ccdcee80cbc943a8c3091283408a89295265adb
ericoborgonove <borgonove@gmail.com>
2025-06-28 11:01:15 -0400

    meio da aula do dia 28/07

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+require('dotenv').config()
+
+const userRoutes = require ('./routes/userRoutes')
+
+const app = express()
+const routes = require('./routes/routes')
+
+app.use(express.json())
+
+app.use('/', routes)
+app.use('/user', userRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send(`404 Não Encontrado`)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log (`App rodando na porta: ${process.env.PORT}`)
+})
+
