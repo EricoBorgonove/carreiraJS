1685801e055128f421b02f35397634ff60ee8a37
ericoborgonove <borgonove@gmail.com>
2025-06-28 11:55:51 -0400

    final da aula do dia 28/07

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,7 @@ router.get('/:id', UserController.getUserById)
 // Buscar por e-mail (/user/buscar/email?email=teste@email.com)
 router.get('/buscar/email', UserController.findByEmail)
 //Buscar por nome (/user/buscar/name?name=ana)
-router.get('/buscar/name', UserController.findByName)
+router.get('/buscar/nome', UserController.findByName)
 // Atualizar usuário
 router.put('/:id', UserController.updateUser)
 // Deletar usuário
