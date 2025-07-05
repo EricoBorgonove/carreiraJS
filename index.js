0c440b28e5da6924eef8618518642342b8bba068
ericoborgonove <borgonove@gmail.com>
2025-07-05 10:25:01 -0400

    meio da aula do dia 05/07

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require("express")
 require('dotenv').config()
 
 const userRoutes = require ('./routes/userRoutes')
+const authRoutes = require ('./routes/authRoutes')
 
 const app = express()
 const routes = require('./routes/routes')
+const authMiddleware = require("./middleware/authMiddleware")
 
 app.use(express.json())
 
 app.use('/', routes)
 app.use('/user', userRoutes)
+app.use('/auth', authRoutes)
+
+app.get('/perfil', authMiddleware, async (req, res) => {
+    res.json({message: `Bem Vindo usuário ${req.usuario.nome}`})
+})
 
 app.use((req, res, next) => {
     res.status(404).send(`404 Não Encontrado`)
